d59e05207ccfe33697559820700e0d1d8b9be9b2
the-coding-beekeeper <117200981+the-coding-beekeeper@users.noreply.github.com>
2023-09-22 05:55:26 +0200

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,7 @@
+function thankYouWindow() {
+    document.getElementById("rating-state-container").style.display = "none";
+    document.getElementById("thank-you-state-container").style.display = "flex";
+}
+function selection(value) {
+    document.getElementById("selected").textContent = value;
+    }
